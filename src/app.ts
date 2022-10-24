3d9654fdfa8eddaedc0a1c83afd0df4065492272
Paulo Sergio <devsergionunes@gmail.com>
2022-10-23 22:25:54 -0300

    adiconando documentação no swagger

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
+import SwaggerUI from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 import { routes } from "./routes";
 import { AppExceptionHandler } from "./middleware/AppException";
 import { InitScriptsDB } from "./db/db";
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerDocument));
 app.use(routes);
 app.use(AppExceptionHandler.handle);
 InitScriptsDB();
